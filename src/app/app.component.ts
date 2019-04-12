bbc01b9bd275a702857069fecf074d3178d596e2
sushmaprs <prssushma@gmail.com>
2019-04-12 16:05:44 +0530

    ngifelse

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngifelse';
+  image=true;
+  course=[1,4,3,4];
+  color="red";
+  heroes="tour";
+  
+}
